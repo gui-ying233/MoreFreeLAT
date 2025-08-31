9b2b26839bbfdfc0ffd2a034efd1e105e128a478
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2025-08-30 20:10:55 -0400

    Remove originalRemoveChild

--- main.user.js
+++ main.user.js
@@ -12,7 +12,6 @@
 
 (() => {
 	"use strict";
-    const originalRemoveChild = Element.prototype.removeChild;
 	document.head.appendChild(
 		Object.assign(document.createElement("style"), {
 			textContent:
